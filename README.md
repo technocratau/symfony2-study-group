01c27f4649bd85d30ccfceded3698cf112ead7b8
VladimirAus <v_roudakov@yahoo.com>
2015-06-04 01:46:15 +1000

    MISC: adding silent script

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ Follow the steps or run
 ```sh
 $ bash -x bin/steps.sh
 ```
+
+Use 
+```sh
+$ bash -x bin/steps_silent.sh
+```
+to recreate all steps silently
---
+++ bin/steps_silent.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Step 1. Installing symfony
+composer create-project symfony/framework-standard-edition lantern 2.7.*
+cd lantern/
+
+# Step 2. Generate LMS bundle
+php app/console generate:bundle --namespace=Technocrat/LMSBundle --format=yml --no-interaction
