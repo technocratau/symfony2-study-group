7d942a44096c175201e988ae19ce91528a8886fd
VladimirAus <v_roudakov@yahoo.com>
2015-06-04 02:08:27 +1000

    STEP 4.2: Generating doctrene entity: Unit

---
+++ lantern/src/Technocrat/LMSBundle/Entity/Unit.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Technocrat\LMSBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Unit
+ */
+class Unit
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var integer
+     */
+    private $number;
+
+    /**
+     * @var \DateTime
+     */
+    private $date_starts;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Unit
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set number
+     *
+     * @param integer $number
+     * @return Unit
+     */
+    public function setNumber($number)
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    /**
+     * Get number
+     *
+     * @return integer 
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+
+    /**
+     * Set date_starts
+     *
+     * @param \DateTime $dateStarts
+     * @return Unit
+     */
+    public function setDateStarts($dateStarts)
+    {
+        $this->date_starts = $dateStarts;
+
+        return $this;
+    }
+
+    /**
+     * Get date_starts
+     *
+     * @return \DateTime 
+     */
+    public function getDateStarts()
+    {
+        return $this->date_starts;
+    }
+}
